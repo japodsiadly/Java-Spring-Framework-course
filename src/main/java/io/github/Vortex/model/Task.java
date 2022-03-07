d676f53e3df277fc535bce87120215566fbc30b0
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-03-07 21:29:06 +0100

    Section 1

---
+++ src/main/java/io/github/Vortex/model/Task.java
@@ -0,0 +1,42 @@
+package io.github.Vortex.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task's description must be not be empty")
+    private String description;
+    private boolean done;
+
+    Task(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(final boolean done) {
+        this.done = done;
+    }
+}
