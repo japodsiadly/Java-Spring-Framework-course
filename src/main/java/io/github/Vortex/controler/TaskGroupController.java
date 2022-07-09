3a2d76ccb00be00c0041bc9de117ac1e36e89584
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-07-09 16:44:57 +0200

    Section 12 finished

--- src/main/java/io/github/Vortex/controler/TaskGroupController.java
+++ src/main/java/io/github/Vortex/controler/TaskGroupController.java
@@ -4,21 +4,23 @@ import io.github.Vortex.logic.TaskGroupService;
 import io.github.Vortex.model.Task;
 import io.github.Vortex.model.TaskRepository;
 import io.github.Vortex.model.projection.GroupReadModel;
+import io.github.Vortex.model.projection.GroupTaskWriteModel;
 import io.github.Vortex.model.projection.GroupWriteModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/groups")
 class TaskGroupController {
-    public static final Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
     private final TaskGroupService taskGroupService;
     private final TaskRepository taskRepository;
 
@@ -27,24 +29,55 @@ class TaskGroupController {
         this.taskRepository = taskRepository;
     }
 
-    @PostMapping
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(produces = MediaType.TEXT_HTML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    String addGroup(
+            @ModelAttribute("group") @Valid GroupWriteModel current,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        taskGroupService.createGroup(current);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Dodano grupę!");
+        return "groups";
+    }
+
+    @PostMapping(params = "addTask", produces = MediaType.TEXT_HTML_VALUE)
+    String addGroupTasks(@ModelAttribute("group") GroupWriteModel current) {
+        current.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+
+    @ResponseBody
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) {
         GroupReadModel result = taskGroupService.createGroup(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-    @GetMapping
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<GroupReadModel>> readAllGroups() {
         return ResponseEntity.ok(taskGroupService.readAll());
     }
 
-    @GetMapping("/{id}")
+    @ResponseBody
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<Task>> readAllTasksFromGroup(@PathVariable int id) {
         return ResponseEntity.ok(taskRepository.findAllByGroup_Id(id));
     }
 
+    @ResponseBody
     @Transactional
-    @PatchMapping("/{id}")
+    @PatchMapping(path = "/{id}")
     ResponseEntity<?> toggleGroup(@PathVariable int id) {
         taskGroupService.toggleGroup(id);
         return ResponseEntity.noContent().build();
@@ -58,5 +91,10 @@ class TaskGroupController {
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ModelAttribute("groups")
+    List<GroupReadModel> getGroups() {
+        return taskGroupService.readAll();
     }
 }
--- src/main/java/io/github/Vortex/model/projection/GroupTaskWriteModel.java
+++ src/main/java/io/github/Vortex/model/projection/GroupTaskWriteModel.java
@@ -2,11 +2,16 @@ package io.github.Vortex.model.projection;
 
 import io.github.Vortex.model.Task;
 import io.github.Vortex.model.TaskGroup;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public class GroupTaskWriteModel {
+    @NotBlank(message = "Task group's description must be not empty")
     private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadline;
 
     public String getDescription() {
@@ -25,7 +30,7 @@ public class GroupTaskWriteModel {
         this.deadline = deadline;
     }
 
-    public Task toTask(TaskGroup group){
+    public Task toTask(TaskGroup group) {
         return new Task(description, deadline, group);
     }
 }
